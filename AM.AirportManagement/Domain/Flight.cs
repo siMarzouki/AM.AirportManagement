cf616792b9b0c57363474d6d9fa817794cdac163
moazmarES <81397619+moazmar@users.noreply.github.com>
2023-03-12 23:55:41 +0100

    Add project files.

---
+++ AM.AirportManagement/Domain/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.AirportManagement.Domain
+{
+    public class Flight
+    {
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime EffectiveArrival { get; set; }
+        public float EstimatedDuration { get; set; }
+        public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+    }
+}
---
+++ AM.AirportManagement/Domain/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.AirportManagement.Domain
+{
+    public class Passenger
+    {
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PassportNumber { get; set; }
+        public string TelNumber { get; set; }
+    }
+}
