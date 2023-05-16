9570134d6b526ced49e0a15235a2ce85454c7cd7
siMarzouki <marzouki.mohamedaziz@esprit.tn>
2023-05-16 14:59:15 +0100

    chapitre 5 partie 1

--- AM.UI.Console/Program.cs
+++ AM.UI.Console/Program.cs
@@ -6,4 +6,4 @@
 //Chap3.Test1();
 //Chap3.Test2();
 //Chap4.Test1();
-Chap4.Test2();
+//Chap4.Test2();
---
+++ AM.UI.Web/Controllers/FlightController.cs
@@ -0,0 +1,93 @@
+﻿using AM.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AM.UI.Web.Controllers
+{
+	public class FlightController : Controller
+	{
+
+		IUnitOfWork unitOfWork;
+		IFlightService flightService;
+		public FlightController(IUnitOfWork unitOfWork,IFlightService flightService) { 
+			this.unitOfWork = unitOfWork;
+			this.flightService = flightService;
+		}
+
+
+		// GET: FlightController
+		public ActionResult Index()
+		{
+			return View(flightService.GetAll());
+		}
+
+		// GET: FlightController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: FlightController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: FlightController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: FlightController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: FlightController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: FlightController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: FlightController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
